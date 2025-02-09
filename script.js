88285cb8552e55883d55d93a3828d16dbc85d7ad
Mo <52931433+mscybr@users.noreply.github.com>
2025-02-09 23:47:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const exec = util.promisify(child_process.exec);
 
 // const express = require('express')
 const app = express()
-const port = 5000
+const port = 5555
 let total_data = {};
 const sites = {};
 
